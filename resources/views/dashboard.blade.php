e1a594e834a5454f4f7b1d9f409b37fbb34ff48e
Erison Francisco de Sales <erisonsales@gmail.com>
2022-10-10 23:35:12 -0300

    Projeto Flow-lite

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,17 @@
+<x-app-layout>
+    {{-- <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot> --}}
+
+
+    <div class="flex">
+        <div class="flex-1 flex-col max-w-xs h-full bg-teal-800">
+            
+        </div>
+    </div>
+
+
+
+</x-app-layout>
